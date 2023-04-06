f1e09bebb09f74ecbcc187aceb487aca9c60472d
XaN0037 <xannodirbek@gmail.com>
2023-04-06 12:40:19 +0500

    Update views.py

--- apps/dashboard/v1/discount/views.py
+++ apps/dashboard/v1/discount/views.py
@@ -10,11 +10,78 @@ class DiscountView(GenericAPIView):
         data = request.data
         product_type = data['type']
 
+        product_id = data['product_id']
+
         nott = 'type' if 'type' not in data else 'product_id' if 'product_id' not in data \
             else 'sub_category_id' if 'sub_category_id' not in data else None
 
         if nott:
             return Response({"Error": f"{nott} kiritilmagan"})
+
+
+        if product_type == 'all' and product_id == 'all':
+            l = []
+            try:
+                for i in Karniz.objects.filter(status=False):
+                    l.append(karniz_format(i))
+            except:
+                l = l
+
+            try:
+                for i in Karona.objects.afilter(status=False):
+                    l.append(karona_format(i))
+            except:
+                l = l
+            try:
+                for i in Kalso.objects.filter(status=False):
+                    l.append(kalso_format(i))
+            except:
+                l = l
+            try:
+                for i in Noj.objects.filter(status=False):
+                    l.append(noj_format(i))
+            except:
+                l = l
+            try:
+                for i in Baget.objects.filter(status=False):
+                    l.append(baget_format(i))
+            except:
+                l = l
+            try:
+                for i in DoriAparat.objects.filter(status=False):
+                    l.append(dori_format(i))
+            except:
+                l = l
+            return Response({"data": l})
+
+
+
+
+
+
+
+
+
+
+
+        if product_type == "karniz":
+            formatt = karniz_format
+            product = Karniz.objects.filter(pk=data['product_id'], category_id=data['sub_category_id'],
+                                            status=True).first()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         if product_type == "karniz":
